49b92d2c5ed6c9633a9a990f6f7f4841a8ec3597
poojitha bijjam <poojitha1589@gmail.com>
2017-08-27 11:21:42 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+static int count;
+int main()
+{
+        int n;
+        printf("enter a number to check if it is prime");
+        scanf("%d",&n);
+        int p=sqrt(n);
+        if(n==2||n==3)
+        {       printf("Given number is  a prime number");
+        }
+         if((n%2)==0||(n%3)==0||(n%5)==0||(n%p)==0)
+        {
+                count++;
+
+        }
+        if(count==0)
+        {
+                printf("Given number is a  prime number");
+        }
+        else
+        {
+                printf("Given number is not a prime number");
+        }
+        return 0;
+}
